07a2e17671b1ec622490b419a75ece5d548e4e65
ssantomaggio <stefano.santomaggio02@gmail.com>
2022-03-03 18:50:18 +0100

    ex.2

---
+++ m1/002-bottle-deposits/second.py
@@ -0,0 +1,5 @@
+small_bottles = float(input("Inserisci il numero di bottiglie piccole"))
+big_bottles = float(input("Inserisci il numero di bottiglie grandi"))
+valore_totale=(small_bottles*0.1)+(big_bottles*0.25)
+
+print("hai guadagnato:",float(valore_totale))
